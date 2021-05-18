96a78e51abd098b7c765dcb7d065afe6a2633500
Xaeart <xaeart@gmail.com>
2021-05-18 17:53:56 -0400

    set up for 3 answers

--- main.go
+++ main.go
@@ -37,12 +37,19 @@ func generateProblem(n int) ([]int, int) {
 //	[0 4 0 5 1 3]   0
 //	[4 0 3 1 2 3]   3
 
-func solveProblem(p []int) int {
+func solveProblem1(p []int) int {
 
 	// ...
 	// Implement the solution here
 	// ...
-	
+
+	return -1
+}
+
+func solveProblem2(p []int) int {
+	return -1
+}
+func solveProblem3(p []int) int {
 	return -1
 }
 
@@ -51,13 +58,33 @@ func main() {
 		// Generate a problem
 		problem, solution := generateProblem(6)
 		fmt.Printf("%v\t", problem)
-		
+
 		// Solve the problem
-		s := solveProblem(problem)
+		s := solveProblem1(problem)
 		fmt.Printf("%d", s)
-		
+
+		// Solve the problem 2nd way
+		answer2 := solveProblem2(problem)
+		fmt.Printf("%d", s)
+
+		// Solve the problem 3rd way.
+		answer3 := solveProblem3(problem)
+		fmt.Printf("%d", s)
+
 		// Print the result
 		if s == solution {
+			fmt.Printf("\tCorrect!")
+		} else {
+			fmt.Printf("\tWRONG!")
+		}
+
+		if answer2 == solution {
+			fmt.Printf("\tCorrect!")
+		} else {
+			fmt.Printf("\tWRONG!")
+		}
+
+		if answer3 == solution {
 			fmt.Printf("\tCorrect!")
 		} else {
 			fmt.Printf("\tWRONG!")
